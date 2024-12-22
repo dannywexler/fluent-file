ce0ae0400814a7401d663a25d8dda629f7503a96
Danny Wexler <dannywexler@gmail.com>
2024-12-21 18:31:17 -0800

    added parse-json to replace JSON.parse, and safe-stable-stringify to replace JSON.stringify

--- src/file/json.ts
+++ src/file/json.ts
@@ -3,12 +3,45 @@ import { zodResult } from "$/common/zod";
 import { AnyFile } from "$/file/any";
 import { readFileText, writeFileText } from "$/file/text.utils";
 import type { Folder } from "$/folder/folder";
+import { fromThrowable } from "neverthrow";
+import parseJson, { type JSONError as JsonParseError } from "parse-json";
+import { configure } from "safe-stable-stringify";
 import type { ZodTypeAny, z } from "zod";
 
+const safeJsonParse = fromThrowable(
+    (text: string, fileName: string) => parseJson(text, fileName),
+    (parseErr) => parseErr as JsonParseError,
+);
+
+const configuredStringify = configure({
+    circularValue: Error,
+    strict: true,
+});
+
+const safeJsonStringify = fromThrowable(
+    (unknownContents: unknown, space?: string | number) => {
+        const results = configuredStringify(unknownContents, null, space);
+        if (results === undefined) {
+            throw new Error("Undefined returned from stringify!!!");
+        }
+        const trimmed = results.trim();
+        if (trimmed.length === 0) {
+            throw new Error("Empty string returned from stringify!!!");
+        }
+        return results;
+    },
+    (stringifyError) => stringifyError as Error,
+);
+
+export class JsonStringifyError extends Error {
+    constructor(path: string, cause: Error) {
+        super(path, { cause });
+        this.name = this.constructor.name;
+    }
+}
+
 export class JsonFile<FileSchema extends ZodTypeAny> extends AnyFile {
-    readonly fileSchema: FileSchema;
-    protected parseUnknown: (text: string) => unknown = JSON.parse;
-    protected stringifyUnknown: (unknownContent: unknown) => string;
+    #fileSchema: FileSchema;
 
     constructor(
         fileSchema: FileSchema,
@@ -16,19 +49,25 @@ export class JsonFile<FileSchema extends ZodTypeAny> extends AnyFile {
         ...extraPathPieces: Strings
     ) {
         super(filePath, ...extraPathPieces);
-        this.fileSchema = fileSchema;
-        this.stringifyUnknown = JSON.stringify;
+        this.#fileSchema = fileSchema;
     }
 
     read() {
-        return readFileText(this.path).andThen((text) =>
-            zodResult(this.fileSchema, this.parseUnknown(text)),
-        );
+        return readFileText(this.path)
+            .andThen((text) => safeJsonParse(text, this.fullName))
+            .andThen((unknownContents) =>
+                zodResult(this.#fileSchema, unknownContents),
+            );
     }
 
-    write(contents: z.infer<FileSchema>) {
-        return zodResult(this.fileSchema, contents)
-            .map(this.stringifyUnknown)
+    write(contents: z.infer<FileSchema>, space?: string | number) {
+        return zodResult(this.#fileSchema, contents)
+            .andThen((unknownContents) =>
+                safeJsonStringify(unknownContents, space).mapErr(
+                    (stringifyError) =>
+                        new JsonStringifyError(this.path, stringifyError),
+                ),
+            )
             .asyncAndThen((stringifiedContents) =>
                 writeFileText(this.path, stringifiedContents),
             );
